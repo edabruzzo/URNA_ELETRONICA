ac14a94dbfa09a83e46f3f4e0d2c88d734cdb1fa
Emmanuel D'Abruzzo <emmanuel.oliveira3@gmail.com>
2019-11-21 12:47:45 -0300

    Update RelatoriosDAO.java

--- src/java/dao/RelatoriosDAO.java
+++ src/java/dao/RelatoriosDAO.java
@@ -1,86 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dao;
+package principal.controllers;
 
-/**
- *
- * @author MARTIN MARIANO
- */
-import java.sql.Connection;
-import java.sql.ResultSet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.RelatoriosDAO;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class RelatoriosDAO extends ConexaoDAO {
+@WebServlet(name = "RelatoriosController", urlPatterns = {"/relatorios"})
+public class RelatoriosController extends HttpServlet {
 
+    RelatoriosDAO relatorioDAO = new RelatoriosDAO();
 
-    public HashMap<String, Integer> listarVotosPorPartido() throws SQLException {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        HashMap<String, Integer> votosPorPartido = new HashMap<>();
-        Connection conn = this.criaConexao();
-        Statement statement = null;
-
-        String sql = "select COUNT(*) AS Votos_Totais,\n" +
-                "P.nomecompleto AS Nome_Partido\n" +
-                "FROM public.tb_voto AS V\n" +
-                "INNER JOIN tb_candidato AS C ON V.id_candidato = C.id_candidato\n" +
-                "INNER JOIN tb_partido AS P ON C.id_partido = P.id_partido\n" +
-                "GROUP BY P.nomecompleto\n" +
-                "ORDER BY COUNT(*);";
-
-        ResultSet rs = null;
-        try {
-            statement = conn.createStatement();
-            rs = statement.executeQuery(sql);
-
-            while (rs.next()) {
-                votosPorPartido.put(rs.getString("Nome_Partido"), rs.getInt("Votos_Totais"));
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            statement.close();
-            rs.close();
-            this.fecharConexao(conn);
-        }
-        return votosPorPartido;
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    public HashMap<String, Integer> listarVotosPorCandidato() throws SQLException {
+        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listaVotosPorPartido = null;
+        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listaVotosPorCandidato = null;
 
-        HashMap<String, Integer> votosPorCandidato = new HashMap<>();
-        Connection conn = this.criaConexao();
-        Statement statement = null;
-
-        String sql = "SELECT COUNT(*) AS Votos_Totais,\n" +
-                "       C.nome_candidato AS Nome_Candidato\n" +
-                "FROM public.tb_voto AS V\n" +
-                "INNER JOIN tb_candidato AS C ON V.id_candidato = C.id_candidato\n" +
-                "GROUP BY C.nome_candidato\n" +
-                "ORDER BY COUNT(*)";
-
-        ResultSet rs = null;
         try {
-            statement = conn.createStatement();
-            rs = statement.executeQuery(sql);
-
-            while (rs.next()) {
-                votosPorCandidato.put(rs.getString("Nome_Candidato"), rs.getInt("Votos_Totais"));
-            }
+            listaVotosPorPartido = relatorioDAO.listarVotosPorPartido();
+            listaVotosPorCandidato = relatorioDAO.listarVotosPorCandidato();
 
         } catch (SQLException ex) {
-            ex.printStackTrace();
-        } finally {
-            statement.close();
-            rs.close();
-            this.fecharConexao(conn);
+            Logger.getLogger(RelatoriosController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return votosPorCandidato;
+
+        request.setAttribute("listaVotosPorCandidato", listaVotosPorCandidato);
+        request.setAttribute("listaVotosPorPartido", listaVotosPorPartido);
+
+        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/relatorios.jsp");
+        dispatcher.forward(request, response);
+
     }
 }
