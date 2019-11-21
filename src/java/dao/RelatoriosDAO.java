ebe11596a0a2c213ab08fca8308976085b68964c
Emmanuel D'Abruzzo <emmanuel.oliveira3@gmail.com>
2019-11-21 12:48:20 -0300

    Update RelatoriosDAO.java

--- src/java/dao/RelatoriosDAO.java
+++ src/java/dao/RelatoriosDAO.java
@@ -1,49 +1,105 @@
-package principal.controllers;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import dao.RelatoriosDAO;
-import java.io.IOException;
+/**
+ *
+ * @author MARTIN MARIANO
+ */
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-@WebServlet(name = "RelatoriosController", urlPatterns = {"/relatorios"})
-public class RelatoriosController extends HttpServlet {
+public class RelatoriosDAO extends ConexaoDAO {
 
-    RelatoriosDAO relatorioDAO = new RelatoriosDAO();
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listarVotosPorPartido() throws SQLException {
 
-    }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> votosPorPartido = new HashMap<>();
+        Connection conn = this.criaConexao();
+        Statement statement = null;
 
-        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listaVotosPorPartido = null;
-        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listaVotosPorCandidato = null;
+        String sql = "SELECT COUNT(*) AS Votos_Totais,\n" +
+                "COALESCE(INITCAP(P.nomecompleto), 'BRANCO') AS Nome_Partido,\n" +
+                "InitCap(E.cargo) AS Cargo\n" +
+                "FROM public.tb_voto AS V\n" +
+                "LEFT JOIN tb_candidato AS C ON V.id_candidato = C.id_candidato\n" +
+                "LEFT JOIN tb_partido AS P ON C.id_partido = P.id_partido\n" +
+                "INNER JOIN tb_eleicao AS E ON E.id_eleicao = V.id_eleicao\n" +
+                "GROUP BY P.nomecompleto, E.cargo\n" +
+                "ORDER BY E.cargo, COUNT(*);";
 
+        ResultSet rs = null;
         try {
-            listaVotosPorPartido = relatorioDAO.listarVotosPorPartido();
-            listaVotosPorCandidato = relatorioDAO.listarVotosPorCandidato();
+            statement = conn.createStatement();
+            rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                String nome_partido = rs.getString("Nome_Partido");
+                int votos_totais = rs.getInt("Votos_Totais");
+                String cargo = rs.getString("cargo");
+
+                votosPorPartido.putIfAbsent(cargo, new ArrayList<>());
+                votosPorPartido.get(cargo).add(new SimpleEntry<>(nome_partido, votos_totais));
+
+            }
 
         } catch (SQLException ex) {
-            Logger.getLogger(RelatoriosController.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } finally {
+            statement.close();
+            rs.close();
+            this.fecharConexao(conn);
         }
+        return votosPorPartido;
+    }
 
-        request.setAttribute("listaVotosPorCandidato", listaVotosPorCandidato);
-        request.setAttribute("listaVotosPorPartido", listaVotosPorPartido);
 
-        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/relatorios.jsp");
-        dispatcher.forward(request, response);
+    public HashMap<String, ArrayList<SimpleEntry<String, Integer>>> listarVotosPorCandidato() throws SQLException {
 
+        HashMap<String, ArrayList<SimpleEntry<String, Integer>>> votosPorCandidato = new HashMap<>();
+        Connection conn = this.criaConexao();
+        Statement statement = null;
+
+        String sql = "SELECT COUNT(*) AS Votos_Totais,\n" +
+                "COALESCE(InitCap(C.nome_candidato), 'BRANCO') AS Nome_Candidato,\n" +
+                "InitCap(E.cargo) AS Cargo\n" +
+                "FROM public.tb_voto AS V\n" +
+                "LEFT JOIN tb_candidato AS C ON V.id_candidato = C.id_candidato\n" +
+                "INNER JOIN tb_eleicao AS E ON E.id_eleicao = V.id_eleicao\n" +
+                "GROUP BY E.cargo, C.nome_candidato\n" +
+                "ORDER BY COUNT(*)";
+
+        ResultSet rs = null;
+        try {
+            statement = conn.createStatement();
+            rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                String nome_candidato = rs.getString("Nome_Candidato");
+                int votos_totais = rs.getInt("Votos_Totais");
+                String cargo = rs.getString("cargo");
+
+                votosPorCandidato.putIfAbsent(cargo, new ArrayList<>());
+                votosPorCandidato.get(cargo).add(new SimpleEntry<>(nome_candidato, votos_totais));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            statement.close();
+            rs.close();
+            this.fecharConexao(conn);
+        }
+        return votosPorCandidato;
     }
 }
